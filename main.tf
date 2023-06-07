9dc02f3c8b332d3d85aa3267cd454c1c6418ae12
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-06-07 11:56:59 -0700

    deployed a lambda function with terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
+# external wiki
+# http://mh-ext-wiki.s3-website-us-west-2.amazonaws.com/
+
 resource "aws_s3_bucket" "ext_wiki" {
   bucket = "mh-ext-wiki"
 }
@@ -65,4 +69,98 @@ resource "aws_s3_object" "ext_wiki" {
   source       = each.value.source_path
   content      = each.value.content
   etag         = each.value.digests.md5
+}
+
+
+# internal wiki
+
+resource "aws_s3_bucket" "int_wiki" {
+  bucket = "mh-int-wiki"
+}
+
+data "aws_iam_policy_document" "s3_allow_lambda" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.int_wiki.arn,
+      "${aws_s3_bucket.int_wiki.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_allow_lambda" {
+  bucket = aws_s3_bucket.int_wiki.id
+  policy = data.aws_iam_policy_document.s3_allow_lambda.json
+}
+
+resource "aws_s3_bucket_website_configuration" "int_wiki" {
+  bucket = aws_s3_bucket.int_wiki.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+module "int_wiki_files" {
+  source   = "hashicorp/dir/template"
+  base_dir = "${path.module}/_int_out"
+}
+
+resource "aws_s3_object" "int_wiki" {
+  for_each = module.int_wiki_files.files
+
+  bucket       = aws_s3_bucket.int_wiki.id
+  key          = each.key
+  content_type = each.value.content_type
+  source       = each.value.source_path
+  content      = each.value.content
+  etag         = each.value.digests.md5
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "authorizer.js"
+  output_path = "authorizer_function_payload.zip"
+}
+
+resource "aws_lambda_function" "authorizer" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename         = "authorizer_function_payload.zip"
+  function_name    = "authorizer"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "authorizer.handler"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+  runtime          = "nodejs18.x"
 }
